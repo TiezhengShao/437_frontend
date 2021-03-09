d63f09b1366bac8cf50912dfdd9dd389d92d13ba
Tiezheng Shao <shaotiez@msu.edu>
2021-03-09 09:52:41 -0600

    Simple Login Page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.use(BootstrapVueIcons);
+
+
+Vue.config.productionTip = false;
+
+// Import Bootstrap and Bootstrap-Vue css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
