6564736c1e9432b690bac91c8378e3d45dc916fd
Zaurbek Zhakupov <zoollhub@gmail.com>
2021-03-15 17:36:38 -0400

    added routing for login and signup, along with some others

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import RoutedApp from './App.vue'
 import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
+// Import Bootstrap and Bootstrap-Vue css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
-
 Vue.use(BootstrapVueIcons);
-
-
 Vue.config.productionTip = false;
 
-// Import Bootstrap and Bootstrap-Vue css
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+new Vue(
+  RoutedApp
+).$mount('#app');
